7162d010e4dc2b668e28357b6b87ea133d73553a
Sam Wang <sam@dgz.sh>
2015-05-25 13:02:46 +0800

    fix bug

--- ci.rb
+++ ci.rb
@@ -4,34 +4,40 @@ require 'json'
 require 'uri'
 require 'yaml'
 require 'byebug'
-
 post '/railgun' do
-  puts '开始执行 CI 脚本'
-  params = JSON.parse(request.env["rack.input"].read)
+  begin
+    puts '开始执行 CI 脚本'
+    params = JSON.parse(request.env["rack.input"].read)
 
-  if params['ref'] != 'refs/heads/develop'
+    if params['ref'] != 'refs/heads/develop'
       puts "#{params['ref']}分支,终止测试"
-    return
-  end
-  path = '/home/deploy/Railgun'
-  `cd #{path} && git pull`
-  `cd #{path} && DISABLE_SPRING=true bundle install`
-  `cd #{path} && RAILS_ENV=test DISABLE_SPRING=true bin/rake db:migrate`
-  `cd #{path} && rake bower:install`
-#  author = get_author path
-  author = params['sender']['username']
-  res = `cd #{path} && RAILS_ENV=test DISABLE_SPRING=true rspec`
-  # get author
-  if res =~ /Finished/
-    total = res[/\d examples/]
-    failure = res[/\d failures/]
-    failure = failure.to_i
-    if failure == 0
-      is_good = true
-    else
-      is_good = false
+      return
     end
-    users.sample.say_to_slack(author, is_good)
+    path = '/home/deploy/Railgun'
+    `cd #{path} && git fetch`
+    `cd #{path} && git reset --hard origin/develop`
+    `cd #{path} && DISABLE_SPRING=true bundle install`
+    `cd #{path} && RAILS_ENV=test DISABLE_SPRING=true bin/rake db:migrate`
+    `cd #{path} && rake bower:install`
+    #  author = get_author path
+    author = params['sender']['username']
+    return if author == 'jaychsu'
+    res = `cd #{path} && RAILS_ENV=test DISABLE_SPRING=true rspec`
+    # get author
+    if res =~ /Finished/
+      total = res[/\d examples/]
+      failure = res[/\d failures/]
+      failure = failure.to_i
+      if failure == 0
+        is_good = true
+      else
+        is_good = false
+      end
+      get_users.sample.say_to_slack(author, is_good)
+    end
+
+  rescue StandardError => e
+    get_users.sample.say_to_slack(author, false)
   end
 end
 
@@ -43,15 +49,25 @@ end
 class User
   attr_accessor :good_words, :bad_words, :url;
 
+  def initialize good_words, bad_words, url
+    @good_words = good_words.shuffle
+    @bad_words  = bad_words.shuffle
+    @url = url
+  end
+
   def say_good
-    good_words.sample
+    word = @good_words.pop
+    @good_words.unshift word
+    word
   end
 
   def say_bad
-    bad_words.sample
+    word = @bad_words.pop
+    @bad_words.unshift word
+    word
   end
 
-  def say_to_slack author, good: true
+  def say_to_slack author, good= true
     if good
       color = 'good'
       msg = say_good
@@ -60,19 +76,21 @@ class User
       msg = say_bad
     end
     msg = "@#{author}: #{msg}"
-    uri = URI(url)
+    uri = URI(@url)
     req = Net::HTTP::Post.new uri.path
     req.body = {username: '', attachments: [color: color, text: msg, username: 'deploy']}.to_json
     res = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) {|http|  http.request req}
   end
 end
 
-users  = []
-Dir.glob('users/*.yml').each do |f|
-  data = YAML.load File.open(f)
-  new_user = User.new
-  new_user.url = data["url"]
-  new_user.good_words = data["good_words"]
-  new_user.bad_words = data["bad_words"]
-  users << new_user
+def get_users
+  if @users.nil?
+    @users  = []
+    Dir.glob('users/*.yml').each do |f|
+      data = YAML.load File.open(f)
+      new_user = User.new data["good_words"],data["bad_words"], data["url"]
+      @users << new_user
+    end
+  end
+  @users
 end
